ea67c1dcad1628039c3159d3e9f7903607b89705
JailsonS <engsoaresfilho@gmail.com>
2021-02-02 18:26:12 -0300

    upload data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { People } from './people';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
+    // template: ``
 })
 export class AppComponent {
-  title = 'test';
+
+  title = 'Title';
+  
+  peopleone = new People(1, 'Jailson');
+  peopletwo = new People(2, 'Joao');
+
+  names = [
+    this.peopleone.name,
+    this.peopletwo.name
+  ];
+
+  bgColor = '#FF00CC';
+
+  // click events
+  changeColor = () => {
+    this.bgColor = '#FF0000';
+  }
 }
+
+// ng generate class nomedaclasse
